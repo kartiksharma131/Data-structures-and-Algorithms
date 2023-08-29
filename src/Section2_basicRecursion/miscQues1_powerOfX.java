33ad2f077fb9adc48b882e108ea97676d66a1d02
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-08-29 09:59:00 +0530

    basic recursion

---
+++ src/Section2_basicRecursion/miscQues1_powerOfX.java
@@ -0,0 +1,22 @@
+package Section2_basicRecursion;
+
+import java.util.Scanner;
+
+public class miscQues1_powerOfX {
+	public static int power(int x,int n) {
+		if(n==0) {
+			return 1;
+		}
+		int ans = x*power(x,n-1);
+		return ans;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int x= scn.nextInt();
+		int n = scn.nextInt();
+		int ans = power(x,n);
+		System.out.println(ans);
+	}
+
+}
---
+++ src/Section2_basicRecursion/miscQues2_numberOfDigits.java
@@ -0,0 +1,21 @@
+package Section2_basicRecursion;
+
+import java.util.Scanner;
+
+public class miscQues2_numberOfDigits {
+	public static int noOfDigits(int n) {
+		if(n==0) {
+			return 0;
+		}
+		int ans = 1+noOfDigits(n/10);
+		return ans;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int ans = noOfDigits(n);
+		System.out.println(ans);
+	}
+
+}
