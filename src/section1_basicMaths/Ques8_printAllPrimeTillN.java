985157d23ce87d47259fc3b5b99f8c5530b66e6c
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-08-29 08:54:13 +0530

    Create Ques8_printAllPrimeTillN.java

---
+++ src/section1_basicMaths/Ques8_printAllPrimeTillN.java
@@ -0,0 +1,30 @@
+package section1_basicMaths;
+
+import java.util.Scanner;
+
+public class Ques8_printAllPrimeTillN {
+	public static void printAllPrime(int n) {
+		for(int i=2;i<n;i++) {
+			int j;
+			boolean flag=true;
+			for(j=2;j<i;j++) {
+				if(i%j==0) {
+					flag=false;
+					break;
+				}
+			}
+			if(flag) {
+				System.out.println(j);
+			}
+			
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		printAllPrime(n);
+
+	}
+
+}
