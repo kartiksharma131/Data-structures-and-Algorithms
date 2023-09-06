d12d4fefecfa7b53926bf64d26c065746d9e375f
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-06 14:02:36 +0530

    Update Ques8_checkPallindromeString.java

--- src/Section2_basicRecursion/Ques8_checkPallindromeString.java
+++ src/Section2_basicRecursion/Ques8_checkPallindromeString.java
@@ -1,10 +1,29 @@
 package Section2_basicRecursion;
 
-public class Ques8_checkPallindromeString {
+import java.util.Scanner;
 
+public class Ques8_checkPallindromeString {
+	public static boolean checkPallindrome(String str,int start,int end) {
+		if(start>=end) {
+			return true;
+		}
+		if(str.charAt(start)!=str.charAt(end)) {
+			return false;
+		}
+		boolean smallAns = checkPallindrome(str, start+1, end-1);
+		return smallAns;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		String str= scn.nextLine();
+		boolean ans = checkPallindrome(str,0,str.length()-1);
+		if(ans) {
+			System.out.println("pallindrome");
+		}
+		else {
+			System.out.println("not pallindrome");
+		}
 	}
 
 }
