4ed42214e77aeacc8d55f9c3e208947b36a3b233
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-08 22:31:28 +0530

    Create Ques5_InsertionSort.java

---
+++ src/section4_Sorting/Ques5_InsertionSort.java
@@ -0,0 +1,32 @@
+package section4_Sorting;
+
+import java.util.Scanner;
+
+public class Ques5_InsertionSort {
+	public static void insertionSort(int [] arr) {
+		for(int i=1;i<arr.length;i++) {
+			int j=i-1;
+			int temp = arr[i];
+			while(j>=0 &&arr[j]>temp ) {
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j+1]=temp;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int [] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		insertionSort(arr);
+		for(int i=0;i<n;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
