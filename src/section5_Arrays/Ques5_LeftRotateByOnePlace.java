f3c4d18cb7fd33aa1fba33b2d5819a9b8fc18c97
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-08 16:28:07 +0530

    Create Ques5_LeftRotateByOnePlace.java

---
+++ src/section5_Arrays/Ques5_LeftRotateByOnePlace.java
@@ -0,0 +1,27 @@
+package section5_Arrays;
+
+import java.util.Scanner;
+
+public class Ques5_LeftRotateByOnePlace {
+	public static void leftRotateByOne(int []arr) {
+		int temp = arr[0];
+		for(int i=1;i<arr.length;i++) {
+			arr[i-1]=arr[i];
+		}
+		arr[arr.length-1]=temp;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int [] arr  =new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = scn.nextInt();
+		}
+		leftRotateByOne(arr);
+		for(int i=0;i<n;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
