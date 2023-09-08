61c314aa72c8235d38bb7eea5cd887b480c668be
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-09 00:05:31 +0530

    Create Ques11_noThatAppearsOnce.java

---
+++ src/section5_Arrays/Ques11_noThatAppearsOnce.java
@@ -0,0 +1,25 @@
+package section5_Arrays;
+
+import java.util.Scanner;
+
+public class Ques11_noThatAppearsOnce {
+	public static int noThatAppearOnce(int [] arr) {
+		int xorr =0;
+		for(int i=0;i<arr.length;i++) {
+			xorr=xorr^arr[i];
+		}
+		return xorr;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int [] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		int ans = noThatAppearOnce(arr);
+		System.out.println(ans);
+	}
+
+}
