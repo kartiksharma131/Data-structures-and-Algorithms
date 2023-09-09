35407e2222d8e9524272da2545d319a60796fb28
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-09 21:03:45 +0530

    Create Ques17_PrintSubarrayWithMaxSum.java

---
+++ src/section5_Arrays/Ques17_PrintSubarrayWithMaxSum.java
@@ -0,0 +1,44 @@
+package section5_Arrays;
+
+import java.util.Scanner;
+
+public class Ques17_PrintSubarrayWithMaxSum {
+
+	public static void kadane(int [] arr) {
+		int currSum=0;
+		int maxi = Integer.MIN_VALUE;
+		int start=0;
+		int ansStart=-1;
+		int ansEnd=-1;
+		for(int i=0;i<arr.length;i++) {
+			if(currSum==0) {
+				start=i;
+			}
+			currSum=currSum+arr[i];
+			if(currSum>maxi) {
+				maxi=currSum;
+				ansStart=start;
+				ansEnd=i;
+			}
+			if(currSum<0) {
+				currSum=0;
+			}
+		}
+		for(int i=ansStart;i<=ansEnd;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n =scn.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		kadane(arr);
+		
+	}
+
+}
