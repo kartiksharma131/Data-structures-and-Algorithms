a2b2a603228f54d7f01c23a2e27fedc53ae7afd9
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-09 21:09:31 +0530

    Create miscQues01_SwapAlternateElements.java

---
+++ src/section5_Arrays/miscQues01_SwapAlternateElements.java
@@ -0,0 +1,34 @@
+package section5_Arrays;
+
+import java.util.Scanner;
+
+public class miscQues01_SwapAlternateElements {
+	public static void swapAlternate(int [] arr) {
+		if(arr.length==0||arr.length==1) {
+			return ;
+		}
+		else {
+			int n = arr.length/2;
+			n=n*2;
+			for(int i=0;i<n;i+=2) {
+				int temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n= scn.nextInt();
+		int [] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		swapAlternate(arr);
+		for(int i=0;i<n;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
