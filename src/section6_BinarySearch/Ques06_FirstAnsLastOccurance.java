5ca239a68de880cddeefb100448a8d0d864f34c2
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-11 01:44:39 +0530

    Update Ques06_FirstAnsLastOccurance.java

--- src/section6_BinarySearch/Ques06_FirstAnsLastOccurance.java
+++ src/section6_BinarySearch/Ques06_FirstAnsLastOccurance.java
@@ -1,6 +1,6 @@
 package section6_BinarySearch;
 
-public class Ques6_FirstAnsLastOccurance {
+public class Ques06_FirstAnsLastOccurance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
