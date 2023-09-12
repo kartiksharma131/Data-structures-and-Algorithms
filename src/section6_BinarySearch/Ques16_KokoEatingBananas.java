7bc0364546b49efb2764f6695ed69702d69bc0d2
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-12 11:39:42 +0530

    Create Ques16_KokoEatingBananas.java

---
+++ src/section6_BinarySearch/Ques16_KokoEatingBananas.java
@@ -0,0 +1,53 @@
+package section6_BinarySearch;
+
+import java.util.Scanner;
+
+public class Ques16_KokoEatingBananas {
+	public static int largestElement(int [] arr) {
+		int largest=Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>largest) {
+				largest=arr[i];
+			}
+		}
+		return largest;
+	}
+	public static int hours(int [] arr,int mid) {
+		int ans = 0;
+		for(int i=0;i<arr.length;i++) {
+			ans+= Math.ceil(arr[i]/mid);
+		}
+		return ans;
+	}
+	public static int koko(int [] arr,int limit) {
+		int low=1;
+		int high = largestElement(arr);
+		int min = Integer.MAX_VALUE;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			int totalHours = hours(arr,mid);
+			if(totalHours<=limit) {
+				min = mid;
+				high=mid-1;
+			}
+			else {
+				low=mid+1;
+			}
+			
+		}
+		return min;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int limit = scn.nextInt();
+		int [] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		int ans = koko(arr,limit);
+		System.out.println(ans);
+	}
+
+}
