7f6639bc34e9ba731dceb2e464139d43411c2637
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-12 10:26:49 +0530

    code corrections

--- src/section6_BinarySearch/Ques11_NoOfRotationsToArray.java
+++ src/section6_BinarySearch/Ques11_NoOfRotationsToArray.java
@@ -2,13 +2,13 @@ package section6_BinarySearch;
 
 import java.util.Scanner;
 
-public class Ques10_minimumInRotatedArray {
+public class Ques11_NoOfRotationsToArray {
 	public static int pivot(int [] arr) {
 		int low=0;
 		int high=arr.length-1;
 		while(low<high) {
-			int mid=(low+high)/2;
-			if(arr[mid]>arr[0]) {
+			int mid = (low+high)/2;
+			if(arr[mid]>=arr[0]) {
 				low=mid+1;
 			}
 			else {
@@ -21,14 +21,13 @@ public class Ques10_minimumInRotatedArray {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		int arr[] =new int[n];
+		int [] arr=new int[n];
 		for(int i=0;i<n;i++) {
 			arr[i]=scn.nextInt();
 		}
-		int peak = pivot(arr);
-		int ans = arr[peak];
+		int peakindex = pivot(arr);
+		int ans = peakindex;
 		System.out.println(ans);
-				
 	}
 
 }
