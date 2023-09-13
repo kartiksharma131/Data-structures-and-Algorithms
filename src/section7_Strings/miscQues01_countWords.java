1f6fdc842eaff67c7d4ea46a484e3525fc32a181
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-13 10:15:17 +0530

    Create miscQues01_countWords.java

---
+++ src/section7_Strings/miscQues01_countWords.java
@@ -0,0 +1,26 @@
+package section7_Strings;
+
+import java.util.Scanner;
+
+public class miscQues01_countWords {
+	public static int countWords(String str) {
+		int ans=0;
+		if(str.length()==0) {
+			return 0;
+		}
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==' ') {
+				ans+=1;
+			}
+		}
+		return ans+1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		int ans = countWords(str);
+		System.out.println(ans);
+	}
+
+}
