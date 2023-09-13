ecfc311f21f5cedb3a2e337f3ca04e341b032c1c
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-13 10:12:03 +0530

    Create IntroductionToStrings.java

---
+++ src/section7_Strings/IntroductionToStrings.java
@@ -0,0 +1,17 @@
+package section7_Strings;
+
+public class IntroductionToStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "abcdefgh";
+		System.out.println(str.charAt(1));
+		System.out.println(str.length());
+		System.out.println(str.substring(3));
+		System.out.println(str.substring(3, 7));
+		System.out.println(str.contains("cde"));
+		
+	}
+
+}
