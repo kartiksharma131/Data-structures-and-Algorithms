5f2739eb4a08e18d991314320491ec5dea792b71
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-16 19:44:01 +0530

    Update miscQues01_TakeInputLinkedList.java

--- src/section08_LinkedLists/miscQues01_TakeInputLinkedList.java
+++ src/section08_LinkedLists/miscQues01_TakeInputLinkedList.java
@@ -23,6 +23,25 @@ public class miscQues01_TakeInputLinkedList {
 		}
 		return head;
 	}
+	public static Node<Integer> takeInputBetter(){
+		Node<Integer> head =null;
+		Node<Integer> tail =null;
+		Scanner scn=new Scanner(System.in);
+		int data=scn.nextInt();
+		while(data!=-1) {
+			Node<Integer> newNode = new Node<Integer>(data);
+			if(head==null) {
+				head = newNode;
+				tail=newNode;
+			}
+			else {
+				tail.next=newNode;
+				tail=tail.next;
+			}
+			data=scn.nextInt();
+		}
+		return head;
+	}
 	public static void printList(Node<Integer> head) {
 		Node<Integer> temp=head;
 		
@@ -34,7 +53,7 @@ public class miscQues01_TakeInputLinkedList {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node<Integer> head= takeInput();
+		Node<Integer> head= takeInputBetter();
 		printList(head);
 	}
 
