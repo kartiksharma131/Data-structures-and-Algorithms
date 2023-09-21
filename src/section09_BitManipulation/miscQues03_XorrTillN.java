2d1b61241c2fc6b3d0c50cc682ce1951f8d8e7bb
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-21 22:47:12 +0530

    Create miscQues03_XorrTillN.java

---
+++ src/section09_BitManipulation/miscQues03_XorrTillN.java
@@ -0,0 +1,29 @@
+package section09_BitManipulation;
+
+import java.util.Scanner;
+
+public class miscQues03_XorrTillN {
+	public static int xorrTillN(int n) {
+		int rem = n%4;
+		if(rem==0) {
+			return n;
+		}
+		else if(rem==1){
+			return 1;
+		}
+		else if(rem==2) {
+			return n+1;
+		}
+		else {
+			return 0;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int ans = xorrTillN(n);
+		System.out.println(ans);
+	}
+
+}
