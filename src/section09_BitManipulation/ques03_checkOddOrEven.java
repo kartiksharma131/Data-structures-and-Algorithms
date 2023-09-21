91ade049b8b15e8a33cac20b9ae38210db393808
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-21 23:15:52 +0530

    Create ques03_checkOddOrEven.java

---
+++ src/section09_BitManipulation/ques03_checkOddOrEven.java
@@ -0,0 +1,20 @@
+package section09_BitManipulation;
+
+import java.util.Scanner;
+
+public class ques03_checkOddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int ans= n&1;
+		if(ans==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+
+}
