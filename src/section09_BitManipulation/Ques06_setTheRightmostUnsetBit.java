7d7eed8ef1a20f3b485468a22ea0ee74af5c8400
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-22 00:07:45 +0530

    Update Ques06_setTheRightmostUnsetBit.java

--- src/section09_BitManipulation/Ques06_setTheRightmostUnsetBit.java
+++ src/section09_BitManipulation/Ques06_setTheRightmostUnsetBit.java
@@ -1,10 +1,27 @@
 package section09_BitManipulation;
 
-public class Ques06_setTheRightmostUnsetBit {
+import java.util.Scanner;
 
+public class Ques06_setTheRightmostUnsetBit {
+	public static int setRightmost(int n) {
+		int mask=1;
+		boolean ans=true;
+		while(ans) {
+			if((n&mask)==0) {
+				return (n|mask);
+			}
+			else {
+				mask=mask<<1;
+			}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int ans = setRightmost(n);
+		System.out.println(ans);
 	}
 
 }
