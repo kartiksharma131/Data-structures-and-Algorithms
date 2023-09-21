d2e577c7917a792df741aff90367c377ba99a4ae
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-21 22:08:20 +0530

    Create misc01_findUniqueNumber.java

---
+++ src/section09_BitManipulation/misc01_findUniqueNumber.java
@@ -0,0 +1,25 @@
+package section09_BitManipulation;
+
+import java.util.Scanner;
+
+public class misc01_findUniqueNumber {
+	public static int findUnique(int [] arr) {
+		int ans =0;
+		for(int i=0;i<arr.length;i++) {
+			ans=ans^arr[i];
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn =new Scanner(System.in);
+		int n = scn.nextInt();
+		int [] arr =new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scn.nextInt();
+		}
+		int ans = findUnique(arr);
+		System.out.println(ans);
+	}
+
+}
