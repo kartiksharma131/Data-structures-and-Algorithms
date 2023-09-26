08c3b056520ef8775cd3d0bd7ec6f2aeedf93657
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-26 23:02:22 +0530

    Create QueueInCollections.java

---
+++ src/section10_StackAndQueues/QueueInCollections.java
@@ -0,0 +1,20 @@
+package section10_StackAndQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueInCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(10);
+		queue.add(20);
+		queue.add(30);
+		System.out.println(queue.poll());
+		System.out.println(queue.size());
+		System.out.println(queue.peek());
+		System.out.println(queue.isEmpty());
+	}
+
+}
