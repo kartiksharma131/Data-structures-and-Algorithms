f5a9fdb5aff579ee266d7ac017767b2561ff6a99
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-09-26 22:07:28 +0530

    Create StackInCollections.java

---
+++ src/section10_StackAndQueues/StackInCollections.java
@@ -0,0 +1,16 @@
+package section10_StackAndQueues;
+
+import java.util.Stack;
+
+public class StackInCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> stack =new Stack<>();
+		stack.push(10);
+		stack.push(20);
+		stack.peek();
+		
+	}
+
+}
