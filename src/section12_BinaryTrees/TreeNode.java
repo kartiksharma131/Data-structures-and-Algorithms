9d8dd67dbcbdcf18266a8c9b06b05f39c9d5d068
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-11-21 09:50:30 +0530

    Binary Tree Basics

---
+++ src/section12_BinaryTrees/TreeNode.java
@@ -0,0 +1,13 @@
+package section12_BinaryTrees;
+
+public class TreeNode<T> {
+	int data;
+	TreeNode<T> left;
+	TreeNode<T> right;
+	public TreeNode(int data){
+		this.data = data;
+		this.left=null;
+		this.right=null;
+	}
+	
+}
